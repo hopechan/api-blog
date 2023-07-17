45f386b67be23dc96bf26316fcc468628901aaca
Esperanza Duenas <esperanza.duenas@unplug.studio>
2023-07-16 21:31:53 -0600

    feat(blog): add crud

---
+++ api/v1/blog.py
@@ -0,0 +1,99 @@
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from werkzeug.exceptions import abort
+
+from api.v1.auth import login_required
+from api.v1.db import get_db
+
+bp = Blueprint("blog", __name__)
+
+
+@bp.route("/")
+def index():
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, content, created_at, author_id"
+        " FROM posts p JOIN users u ON p.author_id = u.id ORDER BY created_at DESC"
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO posts (title, content, author_id) VALUES (?, ?, ?)",
+                (title, content, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/create.html")
+
+
+def get_post(id, check_author=True):
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, content, created_at, author_id"
+            " FROM posts p JOIN users u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post["author_id"] != g.user["id"]:
+        abort(403)
+
+    return post
+
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "UPDATE posts SET title = ?, content = ? WHERE id = ?",
+                (title, content, id),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/update.html", post=post)
+
+
+@bp.route("/<int:id>/delete", methods=("POST",))
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute("DELETE FROM posts WHERE id = ?", (id,))
+    db.commit()
+    return redirect(url_for("blog.index"))
